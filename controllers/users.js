cedc93ad12727617b079c4a352d67211ea0434f2
hackathrone <hackathrone@gmail.com>
2021-10-31 15:30:04 +1100

    adding some packages and signup page

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,14 @@ module.exports = function (_) {
   return {
     SetRouting: function (router) {
       router.get("/", this.indexPage);
+      router.get("/signup", this.getSignUp);
     },
 
     indexPage: function (req, res) {
       return res.render("index", { test: "This is a test" });
+    },
+    getSignUp: function (req, res) {
+      return res.render("signup");
     },
   };
 };
--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const http = require("http");
+const cookieParser = require("cookie-parser");
+const validator = require("express-validator");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const passport = require("passport");
+
 const container = require("./container");
 
 container.resolve(function (users) {
+  mongoose.Promise = global.Promise;
+  mongoose.connect("mongodb://localhost/chatappDB");
   const app = SetupExpress();
 
   function SetupExpress() {
@@ -24,8 +34,23 @@ container.resolve(function (users) {
 
   function ConfigureExpress(app) {
     app.use(express.static("public"));
-    app.set("view engine", ejs);
+    app.use(cookieParser());
+
+    app.set("view engine", "ejs");
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(validator());
+    app.use(
+      session({
+        secret: "thisisasecretkey",
+        resave: true,
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      })
+    );
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
   }
 });
